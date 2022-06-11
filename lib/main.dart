57c471afe648714b19da67f8e96f649a3507b241
Jim Shaw <jimxshaw@gmail.com>
2022-06-11 16:20:10 -0500

    Add GamePage, stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -15,3 +15,26 @@ void main() {
     ),
   );
 }
+
+class GamePage extends StatefulWidget {
+  const GamePage({Key? key}) : super(key: key);
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('BullsEye'),
+          ],
+        ),
+      ),
+    );
+  }
+}
