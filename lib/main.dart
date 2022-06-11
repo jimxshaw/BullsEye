37d24e029b45b06f025e9cc285bddb2ce8c3a67c
Jim Shaw <jimxshaw@gmail.com>
2022-06-11 16:21:46 -0500

    In MaterialApp widget, replace Scaffold with GamgePage widget.

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Bullseye',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Bullseye'),
-        ),
-        body: const Center(
-          child: Text('Hello BullsEye!'),
-        ),
-      ),
+      home: GamePage(),
     ),
   );
 }
